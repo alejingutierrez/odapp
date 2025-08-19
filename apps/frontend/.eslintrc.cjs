63c58778117a823b495af54de82eba6cf1fbc4f7
alejingutierrez <malgutierrezar@gmail.com>
2025-08-19 09:28:57 -0500

    chore: add eslint config for frontend

---
+++ apps/frontend/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['../../.eslintrc.js', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+}
